e11b36244ff68c0ff87f10822ce0bfd1e05589bf
Elex P. <epoisel133@gmail.com>
2025-03-10 13:44:02 -0400

    Add PlantMeasurements component and measurements service

--- frontend/app/components/PlantMeasurements.tsx
+++ frontend/app/components/PlantMeasurements.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useState, useEffect } from 'react';
 import { addMeasurement, getMeasurements, PlantMeasurement } from '../services/measurements';
 import { Line } from 'react-chartjs-2';
